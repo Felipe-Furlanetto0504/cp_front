918f3c9e83ac62f16b1053cca26f2c984ffaa79e
Felipe-Furlanetoo0504 <felipe.furlanetto.4@gmail.com>
2025-10-02 19:32:58 -0300

    fazendo o menu cabecalho, menu e rodape

--- access-control-cp/src/components/Cabecalho/Cabecalho.tsx
+++ access-control-cp/src/components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,10 @@
+import Menu from "../Menu/Menu";
+
 export default function Cabecalho(){
     return(
         <header>
             <h1>Controle de Acesso</h1>
+            <Menu/>
         </header>
     );
 }
