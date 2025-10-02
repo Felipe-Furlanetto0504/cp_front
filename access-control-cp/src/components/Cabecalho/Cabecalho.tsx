1067ad2f843c89b8eeaa9bb4a936beafece8a81f
Felipe-Furlanetoo0504 <felipe.furlanetto.4@gmail.com>
2025-10-02 19:27:31 -0300

    fazendo a pagina error e o cabecalho

---
+++ access-control-cp/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,7 @@
+export default function Cabecalho(){
+    return(
+        <header>
+            <h1>Controle de Acesso</h1>
+        </header>
+    );
+}
--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -1,11 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error from './routes/Error'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      
+    ],
+  },
+]);
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
