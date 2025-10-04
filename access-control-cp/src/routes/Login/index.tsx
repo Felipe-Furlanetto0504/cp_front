1e0b3b7811b8ef28cdc8abefcb0c4504d050bdca
Buenozw <jvictorcastelini2006@gmail.com>
2025-10-04 16:15:14 -0300

    ccomeço da pagina login

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -1,4 +1,32 @@
+import { useForm } from "react-hook-form";
+import type { Login } from "../../types/login";
+
 export default function Login(){
+    const{
+        register,
+        handleSubmit,
+        formState:{errors},
+    } = useForm<Login>();
+
+    const onSubmit = async (data: Login) => {
+        try{
+            const response = await fetch("http://localhost:3000/login",{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            if(!response.ok) throw new Error("Erro ao fazer login");
+
+        } catch(error){
+            console.error(error);
+            alert("Erro ao fazer login");
+        }
+    };
+
+
     return(
         <div>
             <h1>Página de Login</h1>
