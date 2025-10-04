5c919ae680300a938858c53f3449f5c94074cbfe
Buenozw <jvictorcastelini2006@gmail.com>
2025-10-04 15:04:32 -0300

    Login adicionado no db json e pagina de login e cadastro logadas

--- access-control-cp/src/components/Menu/Menu.tsx
+++ access-control-cp/src/components/Menu/Menu.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Menu(){
     return(
         <nav>
-            
+            <Link to="/">Página de Login</Link>
+            <Link to="/cadastro">Página de Cadastro</Link>
         </nav>
     );
 }
