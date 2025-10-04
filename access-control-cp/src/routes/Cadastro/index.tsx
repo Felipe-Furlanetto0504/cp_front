47bb51b242c3640afc95da9bf5daddf4878a33da
Buenozw <jvictorcastelini2006@gmail.com>
2025-10-04 15:26:48 -0300

    começo da cadastro pronta

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -1,4 +1,32 @@
+import { useForm } from "react-hook-form";
+import type { Cadastro } from "../../types/cadastro";
+
 export default function Cadastro(){
+
+    const{
+        register,
+        handleSubmit,
+       formState:{errors},
+    } = useForm<Cadastro>();
+
+    const onSubmit = async (data: Cadastro) => {
+        try{
+            const response = await fetch ("http://localhost:3000/usuario",{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            if(!response.ok) throw new Error("Erro ao cadastrar usuário");
+
+        } catch(error){
+            console.error(error);
+            alert("Erro ao cadastrar usuário");
+        }
+    };
+
     return(
         <div>
             <h1>Página de Cadastro</h1>
