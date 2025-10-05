8c4aedce58d321f5135aaddf4e8f6e880a14b62b
joaocaetano1310 <joaovictorcaetano1310@gmail.com>
2025-10-05 15:08:59 -0300

    css do input do email e nome de cadastro

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -28,26 +28,27 @@ export default function Cadastro(){
     };
 
     return(
-      <section>
-        <div>
-            <h1>Cadastro de Usuário</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label>Email:</label>
+      <section className="bg-[#5d68c9] h-127 w-340">
+        <div className="max-w-md mx-auto mt-12 p-6 border border-gray-300 rounded-xl shadow-md bg-white">
+            <h1 className="text-2xl font-bold text-center mb-6">Cadastro de Usuário</h1>
+            <form  className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+                <label className="mt-4 font-medium" >Email:</label>
                 <input type="email" placeholder="SeuEmail@.com" 
-                {...register("email",{required: "o email é obrigatório"})}/>
+                {...register("email",{required: "o email é obrigatório"})} className="mt-2 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"/>
+
 
                 {errors.email &&(
-                    <span>
+                    <span className="text-red-600 text-sm mt-1" >
                         {errors.email.message}
                     </span>
                 )}
 
-                <label>Nome:</label>
+                <label className="mt-4 font-medium" >Nome:</label>
                 <input type="text" placeholder="SeuNome" 
-                {...register("nome",{required: "o nome é obrigatório"})}/>
+                {...register("nome",{required: "o nome é obrigatório"})} className="mt-2 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"/>
 
                 {errors.nome &&(
-                    <span>
+                    <span className="text-red-600 text-sm mt-1" >
                         {errors.nome.message}
                     </span>
                 )}
